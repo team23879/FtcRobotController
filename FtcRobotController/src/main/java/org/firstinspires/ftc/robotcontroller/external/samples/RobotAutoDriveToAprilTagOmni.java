58a53aca0fd51d445d868db514bab66b57c32cca
RupeshParuchuri <144857014+RupeshParuchuri@users.noreply.github.com>
2023-11-14 18:17:11 -0500

    11142023

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RobotAutoDriveToAprilTagOmni.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RobotAutoDriveToAprilTagOmni.java
@@ -233,7 +233,8 @@ public class RobotAutoDriveToAprilTagOmni extends LinearOpMode
         double rightBackPower    =  x -y +yaw;
 
         // Normalize wheel powers to be less than 1.0
-        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        double max = Math.max(Math.abs(leftFrontPower));
+        max = Math.max(max, Math.abs(rightFrontPower));
         max = Math.max(max, Math.abs(leftBackPower));
         max = Math.max(max, Math.abs(rightBackPower));
 
