e9d6550c3456bfad1d6a5097918d6d8298026a19
RupeshParuchuri <144857014+RupeshParuchuri@users.noreply.github.com>
2023-11-26 13:50:22 -0500

    11262023

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/rightviper.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp
+public class rightviper extends LinearOpMode {
+    private LinearOpMode myOpMode = null;
+    private DcMotor rightSlider = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        rightSlider = hardwareMap.get(DcMotor.class, "rightSliderMotor");
+        waitForStart();
+        telemetry.addData("Before Start", "1");
+        while (true) {
+            if (gamepad1.a) {
+                rightSlider.setDirection(DcMotor.Direction.FORWARD);
+                rightSlider.setPower(0.5);
+                telemetry.addData("In While", "1");
+            }
+            if (gamepad1.b) {
+                rightSlider.setDirection(DcMotor.Direction.REVERSE);
+                rightSlider.setPower(0.5);
+                telemetry.addData("In While", "1");
+            }
+        }
+    }
+}
