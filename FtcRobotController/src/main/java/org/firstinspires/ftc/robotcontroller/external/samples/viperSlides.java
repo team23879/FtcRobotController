7f337730fd7e12647b33b5aad89de6a724e5a2d8
RupeshParuchuri <144857014+RupeshParuchuri@users.noreply.github.com>
2023-11-26 13:50:52 -0500

    11262023

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/viperSlides.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/viperSlides.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
-public class rightviper extends LinearOpMode {
+public class viperSlides extends LinearOpMode {
     private LinearOpMode myOpMode = null;
     private DcMotor rightSlider = null;
 
